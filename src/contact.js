c6d472a67532a6f884695c7db831069daef6f541
sick-carlito <carlb1319@gmail.com>
2020-09-02 02:25:28 +0300

    Added menu and contact pages

--- src/contact.js
+++ src/contact.js
@@ -6,14 +6,32 @@ const contact = (()=>{
 
     let contactContent = document.createElement('div');
     let contactHeading = document.createElement('h2');
+
+    let contactDiv = document.createElement('div');
+    let contactsText = document.createElement('h4');
+    let contactsText1 = document.createElement('h3');
+    let contactsText2 = document.createElement('h3');
     
     const render = (content)=>{
-        contactContent.classList.add("container-fluid")
+        contactContent.classList.add("container-fluid");
         contactContent.id = "contact-content"
 
-        contactHeading.classList.add("text-center")
+        contactHeading.classList.add("text-center");
         contactHeading.textContent = "Contacts";
         contactContent.appendChild(contactHeading);
+
+        contactDiv.classList.add("container-fluid","text-center");
+        contactsText.textContent = "Contact us on:"
+        contactsText1.classList.add("homeheading1");
+        contactsText1.textContent = "carlitos@gmail.com"
+        contactsText1.classList.add("homeheading");
+        contactsText2.textContent = "+254 704 965 601"
+        contactsText2.classList.add("homeheading");
+        contactContent.appendChild(contactsText);
+        contactContent.appendChild(contactsText1);
+        contactContent.appendChild(contactsText2);
+        contactDiv.appendChild(contactContent);
+
 
 
         content.appendChild(contactContent);
